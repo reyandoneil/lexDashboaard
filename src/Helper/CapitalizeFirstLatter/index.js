ca2e18f5dac40da40696c61985dabb00c6e3e187
reyandoneil <reyand@indosafepratama.com>
2022-09-30 13:55:40 +0700

    add

--- src/Helper/CapitalizeFirstLatter/index.js
+++ src/Helper/CapitalizeFirstLatter/index.js
@@ -1,8 +1,21 @@
-export const CapitalizeFirstLatter = (str) => {
-    var splitStr = str.toLowerCase().split(' ')
+// export const CapitalizeFirstLatter = (str) => {
+//     var splitStr = str.toLowerCase().split(' ')
+//     for (var i = 0; i < splitStr.length; i++) {
+//         splitStr[i] = splitStr[i].charAt(0).topUpperCase() + splitStr[i].substring(1)
+//     }
+//     console.log(splitStr,'string');
+//     return splitStr.join(' ')
+// }
+
+function CapitalizeFirstLatterFormt(str) {
+    var splitStr = str.toLowerCase().split(' ');
     for (var i = 0; i < splitStr.length; i++) {
-        splitStr[i] = splitStr[i].charAt(0).topUpperCase() + splitStr[i].substring(1)
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
     }
-    console.log(splitStr,'string');
-    return splitStr.join(' ')
-}
+    // Directly return the joined string
+    return splitStr.join(' '); 
+ }
+
+ export default CapitalizeFirstLatterFormt;
